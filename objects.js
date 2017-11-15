9552a8b071c7f384e5ca46307ad0d010858d0d48
Bushra Taimoor <bushra376@hotmail.com>
2017-11-15 20:32:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,8 +5,9 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
 }
-
+/*
 function removeFromPlaylist (obj, key){
   delete obj.[key];
   return obj;
 }
+*/
